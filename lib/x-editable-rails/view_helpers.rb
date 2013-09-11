659574bfaf8e5992840af262b90515f408143bcd
Manfred Klaffenboeck <fred_klaf@gmx.net>
2013-09-11 16:18:49 +0200

    modified view helper

--- lib/x-editable-rails/view_helpers.rb
+++ lib/x-editable-rails/view_helpers.rb
@@ -6,11 +6,22 @@ module X
           data_url = polymorphic_path(object)
           object = object.last if object.kind_of?(Array)
           if can? :edit, object and xeditable?
-            model = object.class.to_s.downcase
+            model = options[:model] || object.class.to_s.downcase
             model_param = model.gsub('::', '_')
             model_name = model.gsub('::', '/')
-            content_tag :a, href: "#", class: "editable", data: { type: 'text', model: model_param, name: method, url: data_url, 
-              nested: (options[:nested] if options[:nested]), nid: (options[:nid] if options[:nid]), 'original-title' => t("activerecord.attributes.#{model_name}#{"/#{options[:nested].singularize}" if options[:nested]}.#{method}") } do
+            type = options[:type] || "text"
+            content_tag :a, href: "#", 
+            class: "editable", 
+              data: { 
+                type: type.to_s, 
+                model: model_param, 
+                name: method, 
+                url: data_url, 
+                nested: (options[:nested] if options[:nested]), 
+                nid: (options[:nid] if options[:nid]), 
+
+                'original-title' => t("activerecord.attributes.#{model_name}#{"/#{options[:nested].singularize}" if options[:nested]}.#{method}") 
+              } do
                 object.send(method) 
             end
           else
